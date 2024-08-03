fa168d1841bf208770b5945d732024cb7efc5742
daltonvilela <dalton.vilela@gmail.com>
2024-08-03 03:55:57 -0400

    Tela inicial de cadastro e classe de banco de dados

---
+++ db.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = 'localhost';
+$port = '5432';
+$dbname = 'postgres';
+$user = 'postgres';
+$password = 'P1r1l@mp@';
+
+/*
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $conn = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    die('Falha ao conectar ao banco de dados: ' . $e->getMessage());
+}
+*/
+?>
